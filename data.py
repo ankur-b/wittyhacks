8bc9fc95c592bd5609f8e0f41188ef547ca3bc11
Ankur <barveankur29@outlook.com>
2019-01-20 03:45:46 +0530

    Delete data.py

--- data.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-from playsound import playsound
-from io import BytesIO
-my_variable = 'hello'
-mp3_fp = BytesIO(b"")
-tts = gTTS(my_variable, 'en')
-tts.write_to_fp(mp3_fp)
-playsound(mp3_fp)
